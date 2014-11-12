0a0e0dcdfb88eaa31de9f48c4cb6c5b557fe49fb
Miroslav Stampar <miroslav.stampar@gmail.com>
2014-11-12 13:33:02 +0100

    Adding new files

---
+++ pages/config.inc.php
@@ -0,0 +1,4 @@
+===
+<?php echo base64_decode('TGVnYWwgZGlzY2xhaW1lcjoK');?>
+===
+Usage of Damn Small FI Scanner (DSFS) for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program
